e9e15a3847ba02280ff902262ca5b2e7b0e8c7ed
Jelena Cvitanovic <cvitanovic.jelena@gmail.com>
2015-02-23 18:23:38 -0500

    first version of module util.py

---
+++ src/config.py
@@ -0,0 +1,3 @@
+"""Module contains values of configuration variables.
+
+"""
---
+++ src/util.py
@@ -0,0 +1,75 @@
+""" Module util contains helper functions and classes for solving TSP problem.
+
+"""
+
+def City:
+	"""Object representation of a city.
+
+	"""
+
+	def __init__(self, x, y, name):
+		"""Constructor method for class City.
+		
+			Args:
+				x (double) : cartesian coordinate
+				y (double) : cartesian coordinate
+				name (string) : city name
+
+		"""
+		self.x = x
+		self.y = y
+		self.name = name
+
+def Solution:
+		"""Object representation of one particular solution for the TSP problem.
+
+			Attributes:
+				tour_length(double) : length of the tour for the solution
+				city_permutation(list of int) : array of indicies of cities, represents order of visiting each city
+
+		"""
+
+	def __init__(self):
+		"""Constructor method for class Solution.
+
+		"""
+		self.city_permutation = None
+		self.tour_length = None
+
+	def __cmp__(self, other):
+		"""Implementation of comparable method for class Solutions. Objects of class Solution are compared by attribure tour_length.
+
+		"""
+		if self.tour_length > other.tour_length:
+			return 1
+		elif self.tour_length < other.tour_length:
+			return -1
+		else:
+			return 0
+
+def evaluate_solution(solution, city_list, distance_matrix = None):
+	"""Helper method for evaluating fitness of the solution. Method computes tour length for object of class Solution
+
+		Args:
+			solution (Solution) : one instance of TSP solution
+			city_list (list of City) : list of all cities
+			distance_matrix (list of double) : n x n matrix with all distance values from each city to all other
+		Returns:
+			Solution : Solution object with updated length
+
+	"""
+
+def evaluate_population(solutions, city_list, distance_matrix = None):
+	"""Entry point method for evaluating entire population of solutions. Accepts list of solutions and delegates the call to evaluate_solution method.
+
+		Args:
+			solutions (list of Solution) : list of instances of TSP solutions
+			city_list (list of City) : list of all cities
+			distance_matrix (list of double) : n x n matrix with all distance values from each city to all other
+
+	"""
+
+
+
+
+
